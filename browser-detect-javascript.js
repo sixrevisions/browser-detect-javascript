f067fe7c130355ea1b3f136c886aec3880be20d9
sixrevisions <jacob@sixrevisions.com>
2014-07-02 16:03:00 +0800

    initial commit

---
+++ browser-detect-javascript.js
@@ -0,0 +1,42 @@
+// http://sixrevisions.com/javascript/browser-detection-javascript/
+var browserDetect = {
+  init: function () {
+    $('.button').click(function () {
+      // Feature detection: Check if navigator.userAgent exists
+      if (typeof navigator.userAgent === 'undefined') {
+        browserDetect.showInfo('navigator.userAgent is not available in your browser.');
+      } else if ($(this).attr('id') === 'navigator-obj') {
+        browserDetect.navigatorObj();
+      } else {
+        browserDetect.detectJS();
+      }
+    });
+  },
+  // Display navigator.userAgent string in the message area
+  navigatorObj: function () {
+    browserDetect.showInfo(navigator.userAgent);
+  },
+  detectJS: function () {
+    // Load Detect.js only when user requests it
+    $.getScript("Detect.js/detect.min.js")
+    .done(function() {
+      b = detect.parse(navigator.userAgent);
+      // Display the Detect.js parsed properties in the message area
+      browserDetect.showInfo(
+        'Your browser is ' + b.browser.name + '</br>' +
+        'Your device type is ' + b.device.type + '</br>' +
+        'Your operating system is ' + b.os.name + '</br>'
+      );
+    })
+    .fail(function () {
+      // Display notification that Detect.js failed to load
+      showInfo('Couldn\'t load Detect.js');
+    });
+  },
+  // Update message area with the string argument
+  showInfo: function (m) {
+    $('#message-area').html(m);
+  }
+}
+// Call browserDetect object when DOM is ready 
+jQuery(document).ready(browserDetect.init);
